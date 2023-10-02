587ef0cd0f58daa9a4141ffd8ae83e16cba2db14
skrinkook <skrinkook@gmail.com>
2023-10-02 22:07:38 +0200

    guilds scroll bar + setting up first column

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const numberOfServerIcons = 15;
+
+for (i = 0; i < numberOfServerIcons; i++){
+    let div = document.createElement("div");
+    div.classList.add("scroller-server");
+    document.querySelector(".left-scroller-guilds").appendChild(div);
+}
