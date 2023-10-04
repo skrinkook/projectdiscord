985683ef871dfecd12379672d56492d907e47a6e
skrinkook <skrinkook@gmail.com>
2023-10-04 11:28:16 +0200

    updated layout

--- js/main.js
+++ js/main.js
@@ -13,3 +13,16 @@ for (i = 0; i < numberOfChannelsOpened; i++){
     div.classList.add("whitefillblock");
     document.querySelector(".message-column__upperhalf").appendChild(div);
 }
+
+
+fetch('test.json')
+  .then(response => response.json())
+  .then(data => {
+    let dataParse = data.servers[0]
+    let dataParseJSON = JSON.stringify(dataParse);
+    console.log(dataParseJSON)
+    console.log(data.servers[0]);
+  })
+  .catch(error => {
+    console.error(error);
+  });
